dd010fd3c774257af7afedb2338199c0a79e0fd9
Elnur Ruzmanov <80697079+elnurruzmanov@users.noreply.github.com>
2022-09-07 10:05:11 +0300

    I added swiper-slide

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,45 +1,59 @@
-import React from 'react'
-import "./Products.css"
-//Swiper
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
+import React, { useRef, useState } from "react";
+import "./Products.css";
 
+import { Swiper, SwiperSlide } from "swiper/react";
 
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+import "./styles.css";
+// import required modules
+import { Pagination, Navigation } from "swiper";
 //react-router-dom
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
+//images
+import Fabric from "../../images/children's2.svg";
+import Kitchen from "../../images/bed2.svg";
+import Children from "../../images/download2.svg";
 
 const Products = () => {
   return (
-    <div  className='Products'>
+    <div className="Products">
       <div className="container">
         <h1 className="Products-info">Что мы предлагаем?</h1>
         <div className="services">
-          <Link to="/" className='products products1'>Продукты</Link>
-          <Link to="/" className='products products2'>Услуги</Link>
+          <Link to="/" className="products products1">
+            Продукты
+          </Link>
+          <Link to="/" className="products products2">
+            Услуги
+          </Link>
         </div>
         <Swiper
-      spaceBetween={50}
-      slidesPerView={3}
-      onSlideChange={() => console.log('slide change')}
-      onSwiper={(swiper) => console.log(swiper)}
-      // modules={[Navigation, Pagination, Scrollbar, A11y]}
-      // spaceBetween={50}
-      // slidesPerView={3}
-      // navigation
-      // pagination={{ clickable: true }}
-      // scrollbar={{ draggable: true }}
-      // onSwiper={(swiper) => console.log(swiper)}
-      // onSlideChange={() => console.log('slide change')}
-    >
-      <SwiperSlide>Slide 1</SwiperSlide>
-      <SwiperSlide>Slide 2</SwiperSlide>
-      <SwiperSlide>Slide 3</SwiperSlide>
-      <SwiperSlide>Slide 4</SwiperSlide>
-      ...
-    </Swiper>
+          slidesPerView={3}
+          spaceBetween={30}
+          slidesPerGroup={3}
+          loop={true}
+          loopFillGroupWithBlank={true}
+         
+          navigation={true}
+          modules={[Pagination, Navigation]}
+          className="mySwiper"
+        >
+          <SwiperSlide className="swiper-slides"><img src={Fabric} alt="" /></SwiperSlide>
+          <SwiperSlide className="swiper-slides"><img src={Kitchen} alt="" /></SwiperSlide>
+          <SwiperSlide className="swiper-slides"><img src={Children} alt="" /></SwiperSlide>
+          <SwiperSlide className="swiper-slides"><img src={Fabric} alt="" /></SwiperSlide>
+          <SwiperSlide className="swiper-slides"><img src={Kitchen} alt="" /></SwiperSlide>
+          <SwiperSlide className="swiper-slides"><img src={Children} alt="" /></SwiperSlide>
+          <SwiperSlide className="swiper-slides"><img src={Fabric} alt="" /></SwiperSlide>
+          <SwiperSlide className="swiper-slides"><img src={Kitchen} alt="" /></SwiperSlide>
+          <SwiperSlide className="swiper-slides"><img src={Children} alt="" /></SwiperSlide>
+        </Swiper>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Products
+export default Products;
